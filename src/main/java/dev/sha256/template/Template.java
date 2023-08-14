d998066246eab329cf8fb4d18e5f63e6c8cf78d9
Brandon-Lea Price <brandonlea810@gmail.com>
2023-08-15 00:54:18 +0100

    Initial commit

---
+++ src/main/java/dev/sha256/template/Template.java
@@ -0,0 +1,18 @@
+package dev.sha256.template;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+/**
+ * PluginTemplate is a simple template you can use every time you make
+ * a new plugin. This will save you time because you no longer have to
+ * recreate the same skeleton and features each time.
+ * <p>
+ * It uses Foundation for fast and efficient development process.
+ */
+public final class Template extends SimplePlugin {
+
+	@Override
+	protected void onPluginStart() {
+		System.out.println("Lynx is now ready to operate!");
+	}
+}
